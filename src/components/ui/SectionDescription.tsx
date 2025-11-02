335c1cccf05dbf7c289b82ccd6199a0cc8ec7f2f
francky <f@iside.dev>
2025-11-02 04:59:54 +0100

    mobile fix

--- src/components/ui/SectionDescription.tsx
+++ src/components/ui/SectionDescription.tsx
@@ -2,7 +2,7 @@ import parse from 'html-react-parser';
 
 const Sectiontext = ({ text, textColorClass }: { text: string, textColorClass: string }) => {
   return (
-    <p className={`px-8 py-6 md:w-[575px] text-xl rounded-3xl ${textColorClass} border-2 border-white/30 font-bold`}>
+    <p className={`px-8 py-6 md:w-[575px] md:text-xl rounded-3xl ${textColorClass} border-2 border-white/30 font-bold`}>
       {parse(text)}
     </p>
   )
