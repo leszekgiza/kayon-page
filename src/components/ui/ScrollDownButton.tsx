953c1fea563a3a3eccc6ac89fd7cb9ea31c4fce3
francky <f@iside.dev>
2025-11-03 05:32:50 +0100

    fix scroll down button

--- src/components/ui/ScrollDownButton.tsx
+++ src/components/ui/ScrollDownButton.tsx
@@ -18,47 +18,11 @@ const ScrollDownButton = () => {
 
 
   return (
-    // <div id='scroll-down-button' className='fixed xl:relative z-30 top-[85vh] md:top-[70vh] lg:top-[80vh] xl:top-[45vh]'>
-    //   <motion.button
-    //     className="mx-auto h-[57px] w-[57px] z-30 cursor-pointer flex items-center justify-center rounded-[30px] bg-[#bcb7b7] transition-colors duration-300 hover:bg-[#d0d0d0]"
-    //     type="button"
-    //     initial={{ opacity: 0, y: -16 }}
-    //     animate={{ opacity: 1, y: 0, transition: { delay: 0.5, duration: 0.6 } }}
-    //     whileHover={{ y: -8 }}
-    //     transition={{
-    //       y: {
-    //         repeat: Infinity,
-    //         repeatType: "reverse",
-    //         duration: 1.5,
-    //         ease: "easeInOut"
-    //       }
-    //     }}
-    //     onClick={() => {
-    //       const nextSection = document.querySelector('section:nth-of-type(2)');
-    //       nextSection?.scrollIntoView({ behavior: 'smooth' });
-    //     }}
-    //     aria-label="Scroll down"
-    //   >
-    //     <span className="material-symbols-rounded text-[24px] text-white">
-    //       arrow_cool_down
-    //     </span>
-    //   </motion.button>
-    //   <Image
-    //     src="/central-shape.svg"
-    //     className='mt-4 2xl:mt-14 mx-auto'
-    //     alt=""
-    //     width={49}
-    //     height={177}
-    //     priority
-    //     draggable={false}
-    //   />
-    // </div>
-
     <AnimatePresence>
       {isVisible && (
         <motion.div
           id="scroll-down-button"
-          className="fixed xl:relative z-30 top-[85vh] md:top-[70vh] lg:top-[80vh] xl:top-[45vh]"
+          className="fixed z-30 left-1/2 translate-x-[-50%] top-[85vh] md:top-[60vw] lg:top-[45vw] xl:top-[35vw] 2xl:top-[70vh]"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
