e3e17fede778eb8054e6ce689215e211d497a0e9
francky <f@iside.dev>
2025-11-03 07:51:04 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={`${montserrat.variable} font-sans antialiased`}>
         <LanguageProvider>
