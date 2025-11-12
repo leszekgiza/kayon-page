2625852e033f02a97a46f56140c36f02e5cbdaed
francky <f@iside.dev>
2025-11-12 19:46:57 +0100

    fix hero simple

--- src/components/sections/HeroSimple.tsx
+++ src/components/sections/HeroSimple.tsx
@@ -5,11 +5,9 @@ import HeroSimpleCircle from "../ui/HeroSimpleCircle";
 const HeroSimple = ({ title, desc }: { title: string, desc: string }) => {
   return (
     <section className="relative overflow-hidden bg-[#343432] text-white">
-      <HeroSimpleCircle />
-
-      <div className="px-4 py-10 pt-32 md:pt-32 lg:pt-10 lg:px-10 xl:px-20 xl:pt-20 2xl:px-0 max-w-[1440px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:gap-4">
+      <div className="relative z-10 px-4 py-10 pt-32 md:pt-32 lg:pt-10 lg:px-10 xl:px-20 xl:pt-20 2xl:px-0 max-w-[1440px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:gap-4">
         <div className="lg:w-1/2">
-          <KayonLogo className="h-[40px] w-auto md:h-[60px] lg:h-[76px]" />
+          <KayonLogo className="h-auto w-[80%] xl:h-[76px] xl:w-auto md:h-[60px] lg:h-[76px]" />
         </div>
 
         <div className="max-lg:order-last mx-2 xl:mx-24 lg:min-h-[550px] flex flex-col justify-end">
@@ -20,12 +18,14 @@ const HeroSimple = ({ title, desc }: { title: string, desc: string }) => {
           <h1 className="whitespace-pre leading-[1.2] text-2xl md:text-3xl lg:text-4xl text-center lg:text-left">
             {title}
           </h1>
-          <p className="my-4 p-8 border-2 border-[#747171] rounded-3xl md:text-xl font-bold">
+          <p className="my-4 p-8 border-2 border-[#747171] rounded-3xl md:text-xl font-bold bg-[#343432]">
             {desc}
           </p>
         </div>
 
       </div>
+
+      <HeroSimpleCircle />
     </section>
   )
 };
--- src/components/ui/HeroSimpleCircle.tsx
+++ src/components/ui/HeroSimpleCircle.tsx
@@ -1,14 +1,8 @@
 const HeroSimpleCircle = () => {
   return (
-    <div id="header-simple-circle" className="absolute lg:left-[-855px] xl:left-[-700px] 2xl:left-[-550px] top-[-29px] hidden h-[494px] w-[1420px] items-center justify-center lg:flex">
-      <div className="rotate-180">
-        <div className="relative h-[494px] w-[1420px] rounded-[247px] opacity-20">
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-0 rounded-[247px] border-2 border-solid border-[#eaeaea]"
-          />
-        </div>
-      </div>
+    <div
+      id="header-simple-circle"
+      className="absolute z-0 -top-8 w-[50vw] xl:w-[45vw] min-h-[450px] xl:min-h-[550px] hidden lg:block border-s-0 border-2 border-[#747171] rounded-e-full">
     </div>
   )
 };
