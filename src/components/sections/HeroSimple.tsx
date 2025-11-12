d931290485744bba0dd8b447e10b385d0b3f1de4
francky <f@iside.dev>
2025-11-12 18:52:57 +0100

    add circle

--- src/components/sections/HeroSimple.tsx
+++ src/components/sections/HeroSimple.tsx
@@ -5,18 +5,18 @@ import HeroSimpleCircle from "../ui/HeroSimpleCircle";
 const HeroSimple = ({ title, desc }: { title: string, desc: string }) => {
   return (
     <section className="relative overflow-hidden bg-[#343432] text-white">
-      {/* <HeroSimpleCircle /> */}
+      <HeroSimpleCircle />
 
-      <div className="px-4 py-10 pt-32 md:pt-32 lg:px-10 xl:px-20 2xl:px-0 max-w-[1440px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:gap-4">
+      <div className="px-4 py-10 pt-32 md:pt-32 lg:pt-10 lg:px-10 xl:px-20 xl:pt-20 2xl:px-0 max-w-[1440px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:gap-4">
         <div className="lg:w-1/2">
           <KayonLogo className="h-[40px] w-auto md:h-[60px] lg:h-[76px]" />
         </div>
 
-        <div className="max-lg:order-last mx-2 xl:mx-24 lg:h-full flex flex-col justify-end">
+        <div className="max-lg:order-last mx-2 xl:mx-24 lg:min-h-[550px] flex flex-col justify-end">
           <ScrollDownButtonMinimal />
         </div>
 
-        <div className="mt-8 lg:w-1/2">
+        <div className="mt-8 lg:mt-0 lg:w-1/2">
           <h1 className="whitespace-pre leading-[1.2] text-2xl md:text-3xl lg:text-4xl text-center lg:text-left">
             {title}
           </h1>
--- src/components/ui/HeroSimpleCircle.tsx
+++ src/components/ui/HeroSimpleCircle.tsx
@@ -1,6 +1,6 @@
 const HeroSimpleCircle = () => {
   return (
-    <div id="header-simple-circle" className="absolute lg:left-[-955px] xl:left-[-855px] 2xl:left-[-555px] top-[-29px] hidden h-[494px] w-[1420px] items-center justify-center lg:flex">
+    <div id="header-simple-circle" className="absolute lg:left-[-855px] xl:left-[-700px] 2xl:left-[-550px] top-[-29px] hidden h-[494px] w-[1420px] items-center justify-center lg:flex">
       <div className="rotate-180">
         <div className="relative h-[494px] w-[1420px] rounded-[247px] opacity-20">
           <div
